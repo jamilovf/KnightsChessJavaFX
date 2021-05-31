98d6da9e4180e48d7cdbd0d3ea2719e80d3c56d2
Fuad Jamilov <fdjmlv666@gmail.com>
2021-06-01 00:34:16 +0200

    showPossibleMoves method is tested

--- src/test/java/knightschess/model/ChessBoardStateTest.java
+++ src/test/java/knightschess/model/ChessBoardStateTest.java
@@ -3,6 +3,7 @@ package knightschess.model;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -30,5 +31,26 @@ class ChessBoardStateTest {
         chessBoardState.possibleMoves.add(new Pair(0,0));
         assertFalse(chessBoardState.isGameFinished());
 
+    }
+
+    @Test
+    void showPossibleMoves() {
+        Pair pair = new Pair(3,3);
+        chessBoardState.initializeBoard();
+
+        chessBoardState.chessBoard.get(3).set(3,2);
+        chessBoardState.chessBoard.get(2).set(1,1);
+        chessBoardState.chessBoard.get(5).set(4,3);
+
+       chessBoardState.possibleMoves = chessBoardState.showPossibleMoves(pair);
+       List<Pair> expectedPairs = new ArrayList<>();
+       expectedPairs.add(new Pair(1,2));
+       expectedPairs.add(new Pair(1,4));
+       expectedPairs.add(new Pair(2,5));
+       expectedPairs.add(new Pair(4,1));
+       expectedPairs.add(new Pair(4,5));
+       expectedPairs.add(new Pair(5,2));
+
+       assertTrue(chessBoardState.possibleMoves.containsAll(expectedPairs));
     }
 }
