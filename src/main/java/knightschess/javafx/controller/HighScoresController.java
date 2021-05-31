950359d3a0dbec5b99f2fc720d054c5403773389
Fuad Jamilov <fdjmlv666@gmail.com>
2021-05-31 17:26:53 +0200

    Showing scores in table in HighScoreController is implemented

--- src/main/java/knightschess/javafx/controller/HighScoresController.java
+++ src/main/java/knightschess/javafx/controller/HighScoresController.java
@@ -1,16 +1,28 @@
 package knightschess.javafx.controller;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import knightschess.model.HighScore;
 import knightschess.model.ResultState;
 import util.javafx.ControllerHelper;
+import util.json.JsonHelper;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class HighScoresController {
 
@@ -24,6 +36,41 @@ public class HighScoresController {
     @FXML
     private TableColumn<ResultState, Integer> score;
 
+    @FXML
+    public void initialize(){
+        player.setCellValueFactory(new PropertyValueFactory<>("player"));
+        score.setCellValueFactory(new PropertyValueFactory<>("score"));
+
+        ObjectMapper mapper = new ObjectMapper().registerModule(new JavaTimeModule());
+
+        List<ResultState> resultStateList;
+        ObservableList<HighScore> highScores = FXCollections.observableArrayList();
+
+        File file = JsonHelper.read(this);
+
+        try{
+            if(file.length() != 0){
+                resultStateList = mapper.readValue(file, new TypeReference<List<ResultState>>() {
+                });
+                Map<String,Long> map = resultStateList.stream()
+                        .collect(Collectors.groupingBy(ResultState::getWinner,Collectors.counting()));
+
+                map.entrySet().stream()
+                        .sorted(Map.Entry.<String,Long>comparingByValue().reversed())
+                        .limit(5)
+                        .forEach(entry -> {
+                            HighScore highScore = new HighScore();
+                            highScore.setPlayer(entry.getKey());
+                            highScore.setScore(entry.getValue().intValue());
+                            highScores.add(highScore);
+                        });
+            }
+            tableView.setItems(highScores);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 
     public void backAction(ActionEvent actionEvent) throws IOException {
         Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
---
+++ src/main/java/knightschess/model/HighScore.java
@@ -0,0 +1,10 @@
+package knightschess.model;
+
+import lombok.Data;
+
+@Data
+public class HighScore {
+    private String player;
+    private int score;
+
+}
