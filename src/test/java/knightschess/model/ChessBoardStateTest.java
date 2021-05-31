fe5df59138a67b16924c2fe78ad7b51de87bb056
Fuad Jamilov <fdjmlv666@gmail.com>
2021-05-31 22:52:49 +0200

    ChessBoardStateTest class is create and initializeBoard method is tested

---
+++ src/test/java/knightschess/model/ChessBoardStateTest.java
@@ -0,0 +1,25 @@
+package knightschess.model;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChessBoardStateTest {
+    private ChessBoardState chessBoardState = new ChessBoardState();
+
+    @Test
+    void initializeBoard() {
+        chessBoardState.initializeBoard();
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
+                if(!(i == 0 && j == 0) && !(i == 7 && j == 7)){
+                    assertEquals(0,chessBoardState.chessBoard.get(i).get(j));
+                }
+            }
+        }
+        assertEquals(2,chessBoardState.chessBoard.get(0).get(0));
+        assertEquals(3,chessBoardState.chessBoard.get(7).get(7));
+    }
+}
