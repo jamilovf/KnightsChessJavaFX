8098798632dbe8f47dd33c35e6353cef650f1034
Fuad Jamilov <fdjmlv666@gmail.com>
2021-06-01 19:17:27 +0200

    Log is written for Application class

--- src/main/java/knightschess/javafx/KnightsChessApplication.java
+++ src/main/java/knightschess/javafx/KnightsChessApplication.java
@@ -3,6 +3,7 @@ package knightschess.javafx;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import org.tinylog.Logger;
 import util.javafx.ControllerHelper;
 
 public class KnightsChessApplication extends Application {
@@ -10,6 +11,7 @@ public class KnightsChessApplication extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        Logger.info("Application starts...");
         stage.setTitle("Knights of Chess");
         stage.setResizable(false);
         ControllerHelper.loadAndShowFXML(fxmlLoader,"/fxml/launch.fxml",stage);
